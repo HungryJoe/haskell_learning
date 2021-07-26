e2add99de17576420b6c17e36bf6d97ba54f2ffc
Sam Kagan <skagan@nrao.edu>
2021-07-26 13:19:36 -0600

    Added formatting during class on Thursday

--- exercies_1.hs
+++ exercies_1.hs
@@ -1,17 +1,30 @@
+-- #1
 myLast xs = elementAt xs (myLength xs)
 testMyLast = (myLast [1,2,3,4] == 4) && (myLast ['a'..'z'] == 'z')
 
+-- # 2
 myButLast xs = elementAt xs (myLength xs - 1)
 testMyButLast = (myButLast [1,2,3,4] == 3) && (myButLast ['a'..'z'] == 'y')
 
+-- #3
 elementAt xs idx = head (elementAtRecursive xs (myLength xs) idx)
-elementAtRecursive xs total_len idx = if total_len - myLength (tail xs) == idx then [head xs] else elementAtRecursive (tail xs) total_len idx
+elementAtRecursive xs total_len idx = if total_len - myLength (tail xs) == idx
+                                      then
+                                        [head xs]
+                                      else
+                                        elementAtRecursive (tail xs) total_len idx
 testElementAt = (elementAt [1,2,3] 2 == 2) && (elementAt "haskell" 5 == 'e')
 
+-- #4
 myLength xs = sum [1 | _ <- xs]
 testMyLength = (myLength [123, 456, 789] == 3) && (myLength "Hello, world!" == 13)
 
-myReverse xs = if myLength xs == 1 then xs else myLast xs : myReverse (myInit xs)
+-- #5
+myReverse xs = if myLength xs == 1
+               then
+                    xs
+               else
+                    myLast xs : myReverse (myInit xs)
 testMyReverse = (myReverse "A man, a plan, a canal, panama!" == "!amanap ,lanac a ,nalp a ,nam A") && (myReverse [1,2,3,4] == [4,3,2,1])
 
 myInit xs = if myLength xs == 1 then [] else head xs : myInit (tail xs)
