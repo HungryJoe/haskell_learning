d409702bd8221fa7327ae02d80aeb880d09e507e
Sam Kagan <skagan@nrao.edu>
2021-11-09 16:53:01 -0700

    Added support for multiple files in wc.hs

--- wc.hs
+++ wc.hs
@@ -4,11 +4,14 @@ import qualified Data.ByteString as ByteString
 import System.IO.Error
 import System.Environment
 
-main = catchIOError ioStuff handleError
-
-ioStuff = do
+main = do
     args <- getArgs
-    let Args{doWords=doWords, doLines=doLines, doBytes=doBytes, path=filePath} = parseArgs args
+    let Args{doWords=doWords, doLines=doLines, doBytes=doBytes, paths=filePaths} = parseArgs args
+    mapM (processFile doLines doWords doBytes) filePaths
+
+
+processFile :: Bool -> Bool -> Bool -> FilePath -> IO ()
+processFile doLines doWords doBytes filePath = do
     fileContentsByteString <- ByteString.readFile filePath
     let fileContentsText = decodeUtf8 fileContentsByteString
     putStrLn $ addLines doLines fileContentsText ++
@@ -16,27 +19,26 @@ ioStuff = do
                addBytes doBytes fileContentsByteString ++
                filePath
 
-
-data Args = Args{doWords :: Bool, doLines :: Bool, doBytes :: Bool, path :: FilePath}
+data Args = Args{doWords :: Bool, doLines :: Bool, doBytes :: Bool, paths :: [FilePath]}
 parseArgs :: [String] -> Args
-parseArgs args = allFalseToAllTrue $ foldl go Args{doWords=False, doLines=False, doBytes=False, path=""} args
+parseArgs args = allFalseToAllTrue $ foldl go Args{doWords=False, doLines=False, doBytes=False, paths=[]} args
     where go :: Args -> String -> Args
-          go Args{doWords=words, doLines=lines, doBytes=bytes, path=path} arg = Args{
+          go Args{doWords=words, doLines=lines, doBytes=bytes, paths=paths} arg = Args{
               doWords = words || parsedWords,
               doLines = lines || parsedLines,
               doBytes = bytes || parsedBytes,
-              path = if null path then parsedPath else path
+              paths = paths ++ parsedPath
           }
-              where Args{doWords=parsedWords, doLines=parsedLines, doBytes=parsedBytes, path=parsedPath} = parseArg arg
-          parseArg ('-':flags) = parseFlags flags Args{doWords=False, doLines=False, doBytes=False, path=""}
-          parseArg path = Args{doWords=False, doLines=False, doBytes=False, path=path}
+              where Args{doWords=parsedWords, doLines=parsedLines, doBytes=parsedBytes, paths=parsedPath} = parseArg arg
+          parseArg ('-':flags) = parseFlags flags Args{doWords=False, doLines=False, doBytes=False, paths=[]}
+          parseArg path = Args{doWords=False, doLines=False, doBytes=False, paths=[path]}
           parseFlags [] args = args
           parseFlags ('w':flags) args = parseFlags flags args{doWords=True}
           parseFlags ('l':flags) args = parseFlags flags args{doLines=True}
           parseFlags ('c':flags) args = parseFlags flags args{doBytes=True}
           parseFlags (nonFlag:flags) args = error $ "Illegal option -- " ++ [nonFlag]
           -- Preserve the utility of OR as the folding function for the flags while making default behavior match `wc`
-          allFalseToAllTrue args@Args{doWords=words, doLines=lines, doBytes=bytes, path=path}
+          allFalseToAllTrue args@Args{doWords=words, doLines=lines, doBytes=bytes, paths=_}
             | not words && not lines && not bytes = args{doWords=True, doLines=True, doBytes=True}
             | otherwise = args
 
@@ -54,7 +56,3 @@ addBytes :: Bool -> ByteString.ByteString -> String
 addBytes flag contents
     | flag = show (ByteString.length contents) ++ "\t"
     | otherwise = ""
-
--- Embiggen to handle errors in useful ways
-handleError :: IOError -> IO ()
-handleError err = putStrLn "Oopsie"
