f46042fb5565ad7c23377ca1bc4bde2335216aa0
Sam Kagan <skagan@nrao.edu>
2021-12-06 12:34:21 -0700

    Finished day 4 of Advent of Code

--- advent_of_code_2021/4/day4.hs
+++ advent_of_code_2021/4/day4.hs
@@ -10,17 +10,30 @@ main = do
     let (draws, boards) = parseFile file
     let winner@(winningDraw, winningBoard) = playToWin boards draws
     let solution1 = calculateScore winningDraw winningBoard
-    let ((losingDraw, losingBoard), lenBoards) = playToLose boards draws
-    let solution2 = calculateScore losingDraw losingBoard
+    let (losingDraw, losingBoards) = playToLose boards draws
+    let solution2 = calculateScore losingDraw (head losingBoards)
     print winner
     print solution1
-    print lenBoards
+    print (losingDraw, losingBoards)
     print solution2
 
 
-type BingoBoard = [BingoLine]
+newtype BingoBoard = BingoBoard [BingoLine] deriving Eq
+instance Show BingoBoard where
+    show (BingoBoard []) = "[]"
+    show (BingoBoard lines) = '[' : foldl showLine "" lines ++ "]"
+        where showLine acc line = acc ++ show line ++ ",\n"
+instance Ord BingoBoard where
+    compare (BingoBoard a) (BingoBoard b)
+        | a == b = EQ
+        | otherwise = head compareBBs 
+        where compareBBs = dropWhile (==EQ) $ zipWith compare a b
+deconstructBB :: BingoBoard -> [[BingoSquare]]
+deconstructBB (BingoBoard b) = b
 type BingoLine = [BingoSquare]
-data BingoSquare = BingoSquare{marked :: Bool, value :: Int} deriving (Show, Eq)
+data BingoSquare = BingoSquare{value :: Int, marked :: Bool} deriving (Eq, Ord)
+instance Show BingoSquare where
+    show BingoSquare{marked=m, value=v}= '(' : show m ++ ", " ++ show v ++ ")"
 
 parseFile :: String -> ([Int], [BingoBoard])
 parseFile file = (parseDraws (head lines'), parseBoards (tail lines') [])
@@ -29,46 +42,46 @@ parseFile file = (parseDraws (head lines'), parseBoards (tail lines') [])
 
 parseBoards :: [String] -> [BingoBoard] -> [BingoBoard]
 parseBoards [] boards = boards
-parseBoards ("":one:two:three:four:five:otherBoards) prevBoards = parseBoards otherBoards ([parseBoardLine one, parseBoardLine two, parseBoardLine three, parseBoardLine four, parseBoardLine five] : prevBoards)
+parseBoards ("":one:two:three:four:five:otherBoards) prevBoards = parseBoards otherBoards (BingoBoard [parseBoardLine one, parseBoardLine two, parseBoardLine three, parseBoardLine four, parseBoardLine five] : prevBoards)
     where parseBoardLine line = map (bsFromInt . read) (words line)
           bsFromInt x = BingoSquare{marked=False, value=x}
 parseBoards rest boards = error $ "Failed parsing when boards was " ++ show boards ++ " and rest was " ++ show rest
 
-findNextWinner :: [BingoBoard] -> [Int] -> Maybe (Int, BingoBoard, [BingoBoard], [Int])
+findNextWinner :: [BingoBoard] -> [Int] -> Maybe (Int, [BingoBoard], [BingoBoard], [Int])
 findNextWinner _ [] = Nothing
-findNextWinner [] _ = Nothing
 findNextWinner boards (x:xs)
-    | isJust winner = Just (x, fromJust winner, markedBoards, xs)
+    | not $ null winners = Just (x, map BingoBoard winners, markedBoards, xs)
     | otherwise = findNextWinner markedBoards xs
-    where winner = checkBoards markedBoards
-          checkBoards [] = Nothing
-          checkBoards (board:boards') 
-            | checkBoard board = Just board 
-            | otherwise = checkBoards boards'
+    where winners = checkBoards (map deconstructBB markedBoards) []
+          checkBoards [] winners = winners
+          checkBoards (board:boards') winners
+            | checkBoard board = checkBoards boards' $ board : winners
+            | otherwise = checkBoards boards' winners
           checkBoard board = checkRows board || checkCols board
           checkRows = any (all marked)
           checkCols = any (all marked) . transpose
-          markedBoards = map (markBoard x) boards
+          markedBoards = map (BingoBoard . markBoard x . deconstructBB) boards
           markBoard y = map (map $ updateSquare y)
           updateSquare y square@BingoSquare{marked=marked, value=value}
             | not marked && y == value = square{marked=True}
             | otherwise = square
 
-
-playToWin boards draws = (winningDraw, winningBoard)
+playToWin :: [BingoBoard] -> [Int] -> (Int, BingoBoard)
+playToWin boards draws = (winningDraw, head winningBoard)
     where (winningDraw, winningBoard, _, _) = fromJust $ findNextWinner boards draws
 
-playToLose :: [BingoBoard] -> [Int] -> ((Int, BingoBoard), Int)
-playToLose boards draws = go boards draws [] (length boards)
-    where go :: [BingoBoard] -> [Int] -> [(Int, BingoBoard)] -> Int -> ((Int, BingoBoard), Int)
-          go _ [] winners len = (head winners, len)
-          go [] _ winners len = (head winners, len)
-          go boards' draws' winners len
-            | isJust maybeWinner = go (delete winningBoard markedBoards) remainingDraws ((winningDraw, winningBoard) : winners) (length markedBoards)
-            | otherwise = go boards' [] winners len
+playToLose :: [BingoBoard] -> [Int] -> (Int, [BingoBoard])
+playToLose boards draws = go boards draws []
+    where go :: [BingoBoard] -> [Int] -> [(Int, [BingoBoard])] -> (Int, [BingoBoard])
+          go _ [] winners = head winners
+          go [] _ winners = head winners
+          go boards' draws' winners
+            | isJust maybeWinner = go diffedBoards remainingDraws ((winningDraw, winningBoards) : winners)
+            | otherwise = go boards' [] winners
             where maybeWinner = findNextWinner boards' draws'
-                  (winningDraw, winningBoard, markedBoards, remainingDraws) = fromJust maybeWinner
+                  (winningDraw, winningBoards, markedBoards, remainingDraws) = fromJust maybeWinner
+                  diffedBoards = Set.toList $ Set.difference (Set.fromList markedBoards) (Set.fromList winningBoards)
 
 calculateScore :: Int -> BingoBoard -> Int
 calculateScore draw board = draw * sumUnmarked
-          where sumUnmarked = sum [sum [value square | square <- line, not (marked square)] | line <- board]
+          where sumUnmarked = sum [sum [value square | square <- line, not (marked square)] | line <- deconstructBB board]
