f4792d1db94d12cf140437aa6935ef843e4ddd94
Sam Kagan <skagan@nrao.edu>
2021-12-15 11:19:38 -0700

    Finished 5.2 of Advent of Code

--- advent_of_code_2021/5/day5.hs
+++ advent_of_code_2021/5/day5.hs
@@ -6,11 +6,13 @@ import System.Environment (getArgs)
 main = do
     args <- getArgs
     file <- readFile (head args)
-    let parsedFile = filterOutDiagonals $ parseFile file
-    let freqMap = generateFreqMap parsedFile
-    let solution = countIntersections freqMap
-    -- print freqMap
-    print solution
+    let parsedFile = parseFile file
+    -- let freqMap1 = generateFreqMap $ filterOutDiagonals parsedFile
+    -- let solution1 = countIntersections freqMap1
+    let freqMap2 = generateFreqMap parsedFile
+    let solution2 = countIntersections freqMap2
+    -- print solution1
+    print solution2
 
 
 type Vector = (Point, Point)
@@ -34,10 +36,16 @@ filterOutDiagonals vectors = [vector | vector@(Point{x=x1, y=y1}, Point{x=x2, y=
 generateFreqMap :: [Vector] -> Map.Map Point Int
 generateFreqMap = fillMap (Map.empty :: Map.Map Point Int)
     where fillMap freqMap [] = freqMap
-          fillMap freqMap ((Point{x=x1,y=y1}, Point{x=x2,y=y2}):vectors') = fillMap (addPoints (Set.toList product) freqMap) vectors'
-              where product = Set.map makePoint $ Set.cartesianProduct (Set.fromList listXs) (Set.fromList listYs)
-                    listXs = if x1 < x2 then [x1..x2] else [x2..x1]
-                    listYs = if y1 < y2 then [y1..y2] else [y2..y1]
+          fillMap freqMap ((Point{x=x1,y=y1}, Point{x=x2,y=y2}):vectors') = fillMap (addPoints lineSegment freqMap) vectors'
+              where listXs = if x1 <= x2 then [x1..x2] else [x1, (x1 - 1)..x2]
+                    listYs = if y1 <= y2 then [y1..y2] else [y1, (y1 - 1)..y2]
+                    lineSegment = map makePoint lineSegmentTups
+                    lineSegmentTups
+                        | x1 == x2 = map (x1 `entuple`) listYs
+                        | y1 == y2 = map (`entuple` y1) listXs
+                        | length listXs == length listYs = zip listXs listYs  -- Assume slope of 1
+                        | otherwise = error $ "Invalid vector: " ++ show (x1,y1) ++ " " ++ show (x2, y2)
+                    entuple a b = (a,b)
           addPoints [] freqMap = freqMap
           addPoints (point:points) freqMap
             | Set.member point (Map.keysSet freqMap) = addPoints points $ Map.update (Just . (+1)) point freqMap
