0360e56652c39f02b1f90139d46b784f0e70de89
Sam Kagan <skagan@nrao.edu>
2022-12-05 16:46:42 -0700

    Finished day 2 of Advent of Code 2022

--- advent_of_code/2022/2/day2.hs
+++ advent_of_code/2022/2/day2.hs
@@ -7,27 +7,51 @@ main = do
     let score = calculateTotalScore rounds
     print score
 
+class (Eq a, Bounded a, Enum a) => Circ a where
+    next :: a -> a
+    next x
+        | x == maxBound = minBound
+        | otherwise = succ x
+    prev :: a -> a
+    prev x
+        | x == minBound = maxBound
+        | otherwise = pred x
+
 data RPSMove = Rock | Paper | Scissors deriving (Show, Eq)
+
+instance Enum RPSMove where
+    toEnum 0 = Rock
+    toEnum 1 = Paper
+    toEnum 2 = Scissors
+    fromEnum Rock = 0
+    fromEnum Paper = 1
+    fromEnum Scissors = 2
+
+instance Bounded RPSMove where
+    minBound = Rock
+    maxBound = Scissors
+
+instance Circ RPSMove
+
 instance Ord RPSMove where
-    compare Paper Rock = GT
-    compare Rock Paper = LT
-    compare Scissors Paper = GT
-    compare Scissors Rock = LT
-    compare Rock Scissors = GT
-    compare Paper Scissors = LT
-    compare _ _ = EQ
+    compare a b
+        | b == next a = LT
+        | b == prev a = GT
+        | otherwise = EQ
 
 type RPSRound = (RPSMove, RPSMove)
 
 parseInput :: [String] -> [RPSRound]
 parseInput = map parseLine
-    where parseLine [theirMove, ' ', myMove] = (parseTheirMove theirMove, parseMyMove myMove)
-          parseMyMove 'X' = Rock
-          parseMyMove 'Y' = Paper
-          parseMyMove 'Z' = Scissors
-          parseTheirMove 'A' = Rock
-          parseTheirMove 'B' = Paper
-          parseTheirMove 'C' = Scissors
+parseLine [theirMoveCh, ' ', roundResult] = (theirMove, myMove)
+    where myMove 
+              | roundResult == 'X' = prev theirMove
+              | roundResult == 'Y' = theirMove
+              | roundResult == 'Z' = next theirMove
+          theirMove
+              | theirMoveCh == 'A' = Rock
+              | theirMoveCh == 'B' = Paper
+              | theirMoveCh == 'C' = Scissors
 
 calculateRoundScore :: RPSRound -> Int
 calculateRoundScore (theirMove, myMove) = scoreMove myMove + scoreRound myMove theirMove
