384e1494a98a238d7cb850f28524747060298f30
Sam Kagan <skagan@nrao.edu>
2022-12-05 18:31:38 -0700

    Started 3.2 of Advent 2022, broken

--- advent_of_code/2022/3/day3.hs
+++ advent_of_code/2022/3/day3.hs
@@ -1,6 +1,20 @@
 import Data.Char (isLower, isUpper)
 import System.Environment (getArgs)
-type Rucksack = ([Int], [Int])
+import Data.Set (Set, fromList, intersection, union)
+import Data.Foldable (fold, Foldable (toList))
+import Control.Monad.List (foldM)
+import Data.List
+import qualified Data.Sequence (chunksOf, Seq, fromList)
+
+type Rucksack = (Set Int, Set Int)
+
+newtype SetIntersect a = SetIntersect {getSet :: Set a}
+instance (Ord a) => Semigroup (SetIntersect a) where
+    (<>) :: SetIntersect a -> SetIntersect a -> SetIntersect a
+    (<>) x y = SetIntersect $ getSet x `intersection` getSet y
+
+instance (Ord a) => Monoid (SetIntersect a) where
+    mempty = SetIntersect $ fromList []
 
 main = do
     args <- getArgs
@@ -8,7 +22,9 @@ main = do
     fileContents <- readFile fileName
     let rucksacks = map parseLine $ lines fileContents
     let commonPriorities = findCommonPriorities rucksacks
+    let commonPriorities3 = findCommonPrioritiesN 3 rucksacks
     print $ sum commonPriorities
+    print $ sum commonPriorities3
 
 charToPriority :: Char -> Int
 charToPriority a
@@ -16,12 +32,19 @@ charToPriority a
     | isUpper a = fromEnum a - fromEnum 'A' + 27
 
 parseLine :: String -> Rucksack
-parseLine line = splitAt (div (length line) 2) $ map charToPriority line
+parseLine line = (fromList listA, fromList listB)
+    where (listA, listB) = splitAt (div (length line) 2) $ map charToPriority line
 
 findCommonPriority :: Rucksack -> Int
-findCommonPriority (a:as, bs)
-    | a `elem` bs = a
-    | otherwise = findCommonPriority (as, bs)
+findCommonPriority (as, bs) = head $ toList $ intersection as bs
 
 findCommonPriorities :: [Rucksack] -> [Int]
 findCommonPriorities = map findCommonPriority
+
+findCommonPriorityN :: [Rucksack] -> Int
+findCommonPriorityN sacks = head $ toList $ getSet $ foldMap (SetIntersect . fold) sacks
+
+findCommonPrioritiesN :: Int -> [Rucksack] -> [Int]
+findCommonPrioritiesN n sacks = map findCommonPriorityN $ nestedSeqToNestedList $ Data.Sequence.chunksOf n sacksSeq
+    where nestedSeqToNestedList = toList . fmap toList
+          sacksSeq = Data.Sequence.fromList sacks
